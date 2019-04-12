45e0f17f4fdc8a084a7faf26d65337d24e1d399e
xuliyong <308228893@qq.com>
2019-04-12 17:00:55 +0800

    代码提交第一次

---
+++ demo01/src/com/xly/demo01/Demo01.java
@@ -0,0 +1,22 @@
+package com.xly.demo01;
+/**
+ * 测试使用eclipse 提交代码到GitHub
+ * @author Administrator
+ *
+ */
+public class Demo01 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("hello word");
+		
+		
+		
+	}
+		
+
+}
+	
+	
+
